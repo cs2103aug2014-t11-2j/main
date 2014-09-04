aa5c4ff8d15e79a0f2e8aad5f0ccee60f59fe3ac
Zhengyang <fubaer@hotmail.com>
2014-09-04 15:25:01 +0800

    initial commit

---
+++ src/cs2103aug2014t112j/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package cs2103aug2014t112j.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		greetings();
+	}
+	
+	private static void greetings(){
+		System.out.println("Hello World");
+	}
+
+}
