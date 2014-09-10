562f7d87a07b55cf714ccd709f56f6ebdb6d7c1b
fubaer <fubaer@hotmail.com>
2014-09-11 02:13:16 +0800

    Done: >Appointment Class Title, date, time, description, alarm. duration >Task Class Title, date,start date, start time,end date, end time, description, alarm, duration

--- SparkMoWare/src/Appointment.java
+++ SparkMoWare/src/Appointment.java
@@ -1,4 +1,96 @@
+class Appointment {
 
-public class Appointment {
+	/************** Data members **********************/
+	private static int numAppointment = 0;
+	private String title;
+	private int date;		// date of creation;in the format DDMMYY
+	private int startDate;	// in the format DDMMYY, if inactive null
+	private int startTime;	// in the formate HHMM, if inactive null
+	private int endDate;	// in the format DDMMYY, if inactive null
+	private int endTime;	// in the formate HHMM, if inactive null
+	private String description;
+	private int alarm;		// in the format HHMM, if inactive null
+	private int duration;	// in the formate HHMM
+
+	/************** Constructors **********************/
+	// Default constructor
+	private Appointment() {
+	}
+
+	/**************** Accessors ***********************/
+	private String getTitle() {
+		return title;
+	}
+
+	private int getDate() {
+		return date;
+	}
+
+	private int getStartDate() {
+		return startDate;
+	}
+	
+	private int getStartTime() {
+		return startTime;
+	}
+	
+	private int getEndDate() {
+		return endDate;
+	}
+	
+	private int getEndTime() {
+		return endTime;
+	}
+
+	private String getDescription() {
+		return description;
+	}
+
+	private int getAlarm() {
+		return alarm;
+	}
+	
+	private int getduration() {
+		return duration;
+	}
+
+	/**************** Mutators ************************/
+	private void setTitle(String newTitle) {
+		title = newTitle;
+	}
+
+	public void setDate(int newDate) {
+		date = newDate;
+	}
+
+	public void setStartDate(int newstartDate) {
+		startDate = newstartDate;
+	}
+	
+	public void setStartTime(int newStartTime) {
+		startTime = newStartTime;
+	}
+	
+	public void setEndDate(int newEndDate) {
+		endDate = newEndDate;
+	}
+	
+	public void setEndTime(int newEndTime) {
+		endTime = newEndTime;
+	}
+
+
+	public void setDescription(String newDescription) {
+		description = newDescription;
+	}
+
+	public void setAlarm(int newAlarm) {
+		alarm = newAlarm;
+	}
+	
+	private void setDuration(int newDuration) {
+		duration = newDuration;
+	}
 
 }
+
--- SparkMoWare/src/Task.java
+++ SparkMoWare/src/Task.java
@@ -1,4 +1,59 @@
+class Task {
 
-public class Task {
+	/************** Data members **********************/
+	private static int numTask = 0;
+	private String title;
+	private int date;		// date of creation;in the format DDMMYY
+	private int deadline;	// in the format DDMMYY, if inactive null
+	private String description;
+	private int alarm;		// in the format HHMM, if inactive null
+
+	/************** Constructors **********************/
+	// Default constructor
+	private Task() {
+	}
+
+	/**************** Accessors ***********************/
+	private String getTitle() {
+		return title;
+	}
+
+	private int getDate() {
+		return date;
+	}
+
+	private int getDeadline() {
+		return deadline;
+	}
+
+	private String getDescription() {
+		return description;
+	}
+
+	private int getAlarm() {
+		return alarm;
+	}
+
+	/**************** Mutators ************************/
+	private void setTitle(String newTitle) {
+		title = newTitle;
+	}
+
+	public void setDate(int newDate) {
+		date = newDate;
+	}
+
+	public void setDeadline(int newDeadline) {
+		deadline = newDeadline;
+	}
+
+	public void setDescription(String newDescription) {
+		description = newDescription;
+	}
+
+	public void setAlarm(int newAlarm) {
+		alarm = newAlarm;
+	}
 
 }
+
