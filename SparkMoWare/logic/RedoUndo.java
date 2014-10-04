2779e45f2724c34cc2e367c2be5ee3b28ace1b18
fubaer <fubaer@hotmail.com>
2014-10-04 23:51:49 +0800

    Extracted Storage into separate package, removed undoredo dependency on SparkMoVare

--- SparkMoWare/logic/RedoUndo.java
+++ SparkMoWare/logic/RedoUndo.java
@@ -1,35 +1,40 @@
 package logic;
 
 import java.util.LinkedList;
+import java.util.Stack;
+
+import storage.Storage;
 
 /*
  * Use 2 different stacks to redo or undo the actions respectively
  */
 public class RedoUndo {
 
-	public static String undo() {
+	public static String undo(String filePath, LinkedList<Assignment> buffer, Stack<LinkedList<Assignment>> actionHistory,
+			Stack<LinkedList<Assignment>> actionFuture) {
 
-		if (SparkMoVare.actionHistory.empty()) {
+		if (actionHistory.empty()) {
 			return Message.UNABLE_TO_UNDO;
 		} else {
 			LinkedList<Assignment> newEntry = new LinkedList<Assignment>();
-			deepCopyLL(newEntry,SparkMoVare.actionHistory.peek());
-			SparkMoVare.actionFuture.push(newEntry);
-			deepCopyLL(SparkMoVare.buffer,SparkMoVare.actionHistory.pop());
-			SparkMoVare.saveFile(SparkMoVare.getfilePath());
+			deepCopyLL(newEntry,actionHistory.peek());
+			actionFuture.push(newEntry);
+			deepCopyLL(buffer,actionHistory.pop());
+			Storage.saveFile(filePath, buffer);
 			
 			return Message.UNDO;
 		}
 	}
 
-	public static String redo() {
+	public static String redo(String filePath, LinkedList<Assignment> buffer, Stack<LinkedList<Assignment>> actionHistory,
+			Stack<LinkedList<Assignment>> actionFuture) {
 
-		if (SparkMoVare.actionFuture.empty()) {
+		if (actionFuture.empty()) {
 			return Message.UNABLE_TO_REDO;
 		} else {
-			SparkMoVare.actionHistory.push(SparkMoVare.actionFuture.peek());
-			deepCopyLL(SparkMoVare.buffer, SparkMoVare.actionFuture.pop());
-			SparkMoVare.saveFile(SparkMoVare.getfilePath());
+			actionHistory.push(actionFuture.peek());
+			deepCopyLL(buffer, actionFuture.pop());
+			Storage.saveFile(filePath, buffer);
 			
 			return Message.REDO;
 		}
