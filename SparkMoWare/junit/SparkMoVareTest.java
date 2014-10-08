0b5f5485dccd656afd50b8774501029588f3fa2f
Edward Tan <tan.teck.zhi@gmail.com>
2014-10-08 18:49:48 +0800

    testing

--- SparkMoWare/junit/SparkMoVareTest.java
+++ SparkMoWare/junit/SparkMoVareTest.java
@@ -1,8 +1,11 @@
 package junit;
 
 import logic.Add;
+import logic.Comparator;
 import logic.Filter;
 import logic.Print;
+import logic.RefineInput;
+import logic.Edit;
 
 import org.junit.Test;
 
@@ -36,16 +39,23 @@ public class SparkMoVareTest {
 	@Test
 	public void test(){
 	
-		Add.addAssignment("020819940001", "Task1", 0, " ", " ", "03081994", "2359", false, null);
-		Add.addAssignment("140120000001", "Task2", 0, " ", " ", "20012000", "2359", false, null);
-		Add.addAssignment("140120000002", "Task3", 0, " ", " ", "20012000", "2359", false, null);
-		Add.addAssignment("140919940001", "Appointment", 1, "14091994", "0800", "14091994", "0900", false, null);
-		Add.addAssignment("010620140001", "AppointmentConference", 1, "03062014", "0800", "05062014", "1200", false, null);
-		Add.addAssignment("010919940001", "tentativeTask", 2, " ", " ", "01091994", "1600", false, null);
+		Add.addAssignment("020819940001", "Task1", 0, " ", " ", "03081994", "2359", false, "important", null);
+		Add.addAssignment("140120000001", "Task2", 0, " ", " ", "20012000", "2359", false, null, null);
+		Add.addAssignment("140120000002", "Task3", 0, " ", " ", "20012000", "2359", false, "important", null);
+		Add.addAssignment("140919940001", "Appointment", 1, "14091994", "0800", "14091994", "0900", false, null, null);
+		Add.addAssignment("010620140001", "AppointmentConference", 1, "03062014", "0800", "05062014", "1200", false, null, null);
+		Add.addAssignment("010919940001", "tentativeTask", 2, " ", " ", "01091994", "1600", false, null, null);
 		
-//		System.out.println(Comparator.dateComparator("17021996","17071999"));
+//		System.out.println(Comparator.priorityComparator("important",null));
 		
-		Print.printList(Filter.filterMain("tasks"));
+		String[] refinedUserInput = new String[10];
+		refinedUserInput[8] = "edit";
+		refinedUserInput[1] = "140120000001";
+		refinedUserInput[8] = "priority";
+		refinedUserInput[9] = "important";
+		String edited = Edit.editAssignment(refinedUserInput);
+		System.out.println(edited);
+		Print.display();
 		
 	}
 }
