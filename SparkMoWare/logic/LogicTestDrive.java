1fc17f97fd2b799abf91e685b4012a455453abf0
Varunica2 <varunicas@gmail.com>
2014-10-29 08:48:53 +0800

    Revert "Revert "More test cases added. will be refactoring and adding boundary cases.""
    This reverts commit 59126d8c0bf3c40a819625e2246522a2832cc33e.

--- SparkMoWare/logic/LogicTestDrive.java
+++ SparkMoWare/logic/LogicTestDrive.java
@@ -2,8 +2,14 @@ package logic;
 
 import java.util.LinkedList;
 
+import parser.RefinedUserInput;
+
 
 public class LogicTestDrive {
+	
+		public static String addSomething(RefinedUserInput userInput){
+			return addSomething(userInput);
+		}
 
 		public static String addAssignment(String id, String title, boolean isDone, String priority) {
 			return Add.addAssignment(id, title, isDone, priority);
